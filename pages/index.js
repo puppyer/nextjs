258f2185a8f51d7515a6f14e1a544387b003a980
Charles <1309571292@qq.com>
2019-08-13 17:31:56 +0800

    add head

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
+import Head from 'next/head';
 function Home(){
-  return <div>welcome to Next.js!</div>
+  return (
+    <div>
+      <Head>
+        <title>nextjs study</title>
+        <meta name="viewport" content="initial-scale=1.0,width=device-width"/>
+      </Head>
+      <h1>Welcome to nextjs!</h1>
+    </div>
+  )
 }
 export default Home;
